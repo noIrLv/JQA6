2a9667f7839ef3e9448e6aefaf9c70a13faed048
Михаил Новиков <spore125@gmail.com>
2023-01-31 15:01:03 +0300

    first commit

---
+++ src/main/java/netolgy/ru/stats/StatsService.java
@@ -0,0 +1,70 @@
+package netolgy.ru.stats;
+
+public class StatsService {
+    public int sumAllSales(int[] sales) {
+        int sum = 0;
+        for (int i = 0; i < sales.length; i++) {
+            sum = sum + sales[i];
+        }
+        return sum;
+    }
+
+    public int avgSaleOnMonth(int[] sales) {
+        int avgSale = sumAllSales(sales) / sales.length;
+
+        return avgSale;
+
+    }
+
+    public int maxSales(int[] sales) {
+        int maxMonth = 0;
+        int month = 0;
+        for (long sale : sales) {
+
+            if (sale >= sales[maxMonth]) {
+                maxMonth = month;
+            }
+            month = month + 1;
+        }
+        return maxMonth + 1;
+    }
+
+    public int minSales(int[] sales) {
+        int minMonth = 0;
+        int month = 0;
+        for (int sale : sales) {
+
+            if (sale <= sales[minMonth]) {
+                minMonth = month;
+            }
+            month = month + 1;
+        }
+        return minMonth + 1;
+    }
+    public int monthLessAvg(int[] sales){
+        int monthLess = 0;
+        for (int sale : sales){
+            if (sale < avgSaleOnMonth(sales))
+                monthLess++;
+        }
+        return monthLess;
+    }
+
+    public int monthMoreAvg(int[] sales){
+        int monthLess = 0;
+        for (int sale : sales){
+            if (sale > avgSaleOnMonth(sales))
+                monthLess++;
+        }
+        return monthLess;
+
+
+
+
+
+}
+}
+
+
+
+
---
+++ src/test/java/StatsServiceTest.java
@@ -0,0 +1,79 @@
+import netolgy.ru.stats.StatsService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class StatsServiceTest {
+    @Test
+    public void allSalesSum() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedSales = 180;
+        int actualSales = service.sumAllSales(sales);
+
+        Assertions.assertEquals(expectedSales, actualSales);
+    }
+
+    @Test
+    public void avgSaleOnMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedAvg = 15;
+        int actualAvg= service.avgSaleOnMonth(sales);
+
+        Assertions.assertEquals(expectedAvg, actualAvg);
+
+    }
+
+    @Test
+    public void maxMonth() {
+        StatsService service = new StatsService();
+        int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMax = 8;
+        int actualMax = service.maxSales(sales);
+
+        Assertions.assertEquals(expectedMax, actualMax);
+
+    }
+    @Test
+    public void minMonth() {
+        StatsService service = new StatsService();
+        int [] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expectedMin = 9;
+        int actualMin = service.minSales(sales);
+
+        Assertions.assertEquals(expectedMin, actualMin);
+
+    }
+
+    @Test
+    public void lessThanAvgMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actualLess = service.monthLessAvg(sales);
+
+        Assertions.assertEquals(expected, actualLess);
+
+
+    }
+
+    @Test
+    public void moreThanAvgMonth() {
+        StatsService service = new StatsService();
+        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
+
+        int expected = 5;
+        int actualMore = service.monthMoreAvg(sales);
+
+        Assertions.assertEquals(expected, actualMore);
+
+
+    }
+
+
+}
